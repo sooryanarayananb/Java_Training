4a7e10248f2b05946c82dc52f77e603d90b229f5
sooryanarayanan <sooryanarayanan@qburst.com>
2021-03-17 19:38:06 +0530

    Enum example added

---
+++ Java/EnumExample.java
@@ -0,0 +1,47 @@
+// Every enum extends from Enum class which is in Java. 
+// An enum is a special "class" that represents a group of constants 
+// (unchangeable variables, like final variables).
+// enum can be inside or outside a class. 
+// enum can also have attributes, methods as same as classes. 
+
+// enum is converted into:
+// class Mobile {
+//   static final Mobile APPLE = new Mobile();  
+// }
+
+
+enum Mobile {
+
+	APPLE(400), SAMSUNG(200), MI;
+
+	int price; 
+
+	Mobile(){
+		price = 80;
+	}
+
+	Mobile(int p){
+		price = p;
+	}
+
+	public int getPrice(){
+		return price;
+	}
+
+}
+
+class EnumExample{
+
+	public static void main(String args[]){
+
+		System.out.println(Mobile.APPLE.getPrice());
+
+		Mobile m[] = Mobile.values();
+
+		for(Mobile i: m){
+			System.out.println(i.getPrice());
+		}
+
+	}
+
+}
